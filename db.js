0083d748877e13576c6165f97eafae0a37776c6a
Aman kumar <108401753+Amankumar968@users.noreply.github.com>
2024-05-20 11:16:58 +0530

    first Commit

---
+++ db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// const mongoURL=process.env.MONGODB_URL_LOCAL
+const mongoURL = process.env.MONGODB_URL;
+
+if (!mongoURL) {
+    throw new Error('MONGODB_URL is not defined');
+}
+
+mongoose.connect(mongoURL)
+
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('MongoDB connection successful');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+module.exports = db;
